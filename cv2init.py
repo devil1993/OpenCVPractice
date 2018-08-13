7ee9cdf2af5e6a8efb299d6baa9f4fee1bf21ca1
devil1993 <samanwayghatak@gmail.com>
2018-08-13 21:08:10 +0530

    Added color selection

--- cv2init.py
+++ cv2init.py
@@ -108,13 +108,13 @@ import matplotlib.pyplot as plt
 
 # ================================================STEP6=======================================================
 
-# img = cv2.imread('1.JPG')
+# img = cv2.imread('d.jpg')
 # retval, threshold = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
 
 # grayscaled = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # retval1, threshold1 = cv2.threshold(grayscaled, 120, 255, cv2.THRESH_BINARY)
 
-# gaus = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115,15)
+# gaus = cv2.adaptiveThreshold(grayscaled,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 115, 15)
 
 # cv2.imshow('original',img)
 # cv2.imshow('threshold', threshold)
@@ -125,3 +125,27 @@ import matplotlib.pyplot as plt
 # cv2.destroyAllWindows()
 
 # =================================================STEP7==================================
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	_, frame = cap.read()
+	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+
+	lower_yellow = np.array([20,90,0])
+	upper_yellow = np.array([40,255,255])
+
+	mask = cv2.inRange(hsv, lower_yellow,upper_yellow)
+	res = cv2.bitwise_and(frame, frame, mask = mask)
+
+	cv2.imshow('frame',frame)
+	cv2.imshow('mask',mask)
+	cv2.imshow('res',res)
+
+	k = cv2.waitKey(5) & 0xFF
+	if(k == 27):
+		break
+
+cv2.destroyAllWindows()
+cap.release()
